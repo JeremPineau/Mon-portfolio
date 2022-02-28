87fe5b2bf83e35cac91f568241374db69b990c81
JeremPineau <pi.jeremieoutlook.fr>
2022-02-28 20:44:02 +0100

    Fix

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NotFound from "./pages/NotFound";
 import Homepage from "./pages/Homepage";
 import AboutMe from "./pages/AboutMe";
 import MyAchievements from "./pages/MyAchievements";
@@ -10,7 +9,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<NotFound />} />
         <Route path='/' element={<Homepage />} />
         <Route path='/about-me' element={<AboutMe />} />
         <Route path='/my-achievements' element={<MyAchievements />} />
