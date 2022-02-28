e766cc3a2c1bc81703b5ae01e21d681c2157032a
JeremPineau <pi.jeremieoutlook.fr>
2022-02-28 19:33:09 +0100

    Add: elements page d'accueil

--- mon-app/src/pages/Homepage.js
+++ mon-app/src/pages/Homepage.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Jeremie from '../assets/jeremie.png';
 
 const Homepage = () => {
     return (
         <div className="homepage">
             <Navigation />
-            <h1>Accueil</h1>
+            <h1>Bienvenue sur mon portfolio !</h1>
+            <div id="presentation">
+                <div id="jeremie">
+                    <img src={Jeremie} alt="jeremie" />
+                </div>
+                <div id="description">
+                    <h2>Jérémie Pineau</h2>
+                    <h3></h3>
+                    <p>Passionné par le développement web, je souhaiterais vivre de ma passion.</p>
+                    <p>Mes diverses expériences m'ont apporté beaucoup de connaissances que je souhaite, aujourd'hui, mettre à contribution en intégrant une équipe de devéloppeurs.</p>
+                    <p>Motivation, perfectionnisme et soif d'apprendre sont les adjectifs qui me définissent le mieux, comme en témoigne mon parcours scolaire plutôt atypique.</p>
+                </div>
+            </div>
         </div>
     );
 };
