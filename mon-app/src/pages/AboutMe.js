3e2ee798fa7ffc12f4a583677c0f2e4a99a212db
JeremPineau <pi.jeremieoutlook.fr>
2022-03-01 10:38:46 +0100

    Add: ajout element page a propos de moi +Fix

--- mon-app/src/pages/AboutMe.js
+++ mon-app/src/pages/AboutMe.js
@@ -19,7 +19,38 @@ import Wienerberger from '../assets/about-me/experiences/production-worker/wiene
 import AgentDeProduction from '../assets/about-me/experiences/production-worker/agent-de-production.jpg';
 import NordFranceConstructions from '../assets/about-me/experiences/internship1/nord-france-constructions.png';
 
-
+/* skills */
+import France from '../assets/about-me/skills/languages/france.png';
+import Angleterre from '../assets/about-me/skills/languages/angleterre.jpg';
+import USA from '../assets/about-me/skills/languages/usa.jpg';
+import Espagne from '../assets/about-me/skills/languages/espagne.jpg';
+import CCpp from '../assets/about-me/skills/programming-languages/programming/c-cpp.png';
+import CSharp from '../assets/about-me/skills/programming-languages/programming/csharp.png';
+import Python from '../assets/about-me/skills/programming-languages/programming/python.png';
+import Java from '../assets/about-me/skills/programming-languages/programming/java.png';
+import HtmlCss from '../assets/about-me/skills/programming-languages/web/html-css.png';
+import Php from '../assets/about-me/skills/programming-languages/web/php.png';
+import Laravel from '../assets/about-me/skills/programming-languages/web/laravel.png';
+import Angular from '../assets/about-me/skills/programming-languages/web/angular.png';
+import ReactJS from '../assets/about-me/skills/programming-languages/web/react-js.png';
+import JavaScript from '../assets/about-me/skills/programming-languages/web/javascript.png';
+import TypeScript from '../assets/about-me/skills/programming-languages/web/typescript.png';
+import XAML from '../assets/about-me/skills/programming-languages/graphical-interfaces/xaml.png';
+import VisualBasic from '../assets/about-me/skills/programming-languages/graphical-interfaces/visual-basic.png';
+import PythonTkinter from '../assets/about-me/skills/programming-languages/graphical-interfaces/python-tkinter.jpg';
+import MySQL from '../assets/about-me/skills/programming-languages/databases/mysql.png';
+import PostgreSQL from '../assets/about-me/skills/programming-languages/databases/postgresql.png';
+import NoSQL from '../assets/about-me/skills/programming-languages/databases/nosql.png';
+import VisualStudioCode from '../assets/about-me/skills/softwares/programming/visual-studio-code.png';
+import VisualStudio from '../assets/about-me/skills/softwares/programming/visual-studio.png';
+import IntelliJ from '../assets/about-me/skills/softwares/programming/intellij.png';
+import PhpStorm from '../assets/about-me/skills/softwares/programming/php-storm.png';
+import Git from '../assets/about-me/skills/softwares/programming/git.png';
+import Trello from '../assets/about-me/skills/softwares/organization-tools/trello.png';
+import JiraSoftware from '../assets/about-me/skills/softwares/organization-tools/jira-software.png';
+import AutoCAD from '../assets/about-me/skills/softwares/architecture/autocad.png';
+import RevitArchitecture from '../assets/about-me/skills/softwares/architecture/revit-architecture.png';
+import SketchUp from '../assets/about-me/skills/softwares/architecture/sketchup.png';
 
 const AboutMe = () => {
     return (
@@ -30,37 +61,37 @@ const AboutMe = () => {
                 
                 <h2>Mon parcours</h2>
                 <div id="career-list">
-                    <div class="career">
+                    <div id="licence">
                         <h3>2021 - 2022</h3>
                         <h3>EPSI, Lille</h3>
-                        <img class="pic" src={Epsi} alt="epsi" />	
-                        <img class="pic" src={Devops} alt="devops" />
+                        <img src={Epsi} alt="epsi" width={'30%'} />	
+                        <img src={Devops} alt="devops" width={'30%'} />
                         <p>Bachelor informatique</p>
                         <p class="italic">Concepteur développeur d'application / Concepteur intégrateur DEVOPS</p>
                     </div>
-                    <div class="career">
+                    <div id="dut">
                         <h3>2018 - 2021</h3>
                         <h3>IUT de Lens et d'Amiens</h3>
-                        <img id="iut-amiens" src={IUTAmiens} alt="iut-amiens" />
-                        <img class="pic" src={IUTLens} alt="iut-lens" />	
-                        <img class="pic" src={DUTInformatique} alt="dut-informatique" />
+                        <img src={IUTAmiens} alt="iut-amiens" width={'15%'} />
+                        <img src={IUTLens} alt="iut-lens" width={'30%'} />	
+                        <img src={DUTInformatique} alt="dut-informatique" width={'30%'} />
                         <p>DUT informatique</p>
                     </div>
-                    <div class="career">
+                    <div id="bts">
                         <h3>2014 - 2016</h3>
                         <h3>Lycée Jean Prouvé, Lomme</h3>
-                        <img class="pic" src={LyceeJeanProuve} alt="lycee-jean-prouve" />	
-                        <img id="bts-batiment" src={BTSbatiment} alt="bts-batiment" />
+                        <img src={LyceeJeanProuve} alt="lycee-jean-prouve" width={'30%'} />	
+                        <img src={BTSbatiment} alt="bts-batiment" width={'20%'} />
                         <p>BTS bâtiment</p>
                     </div>
                 </div>
 
                 <h2>Mes experiences professionnelles</h2>
                 <div id="experiences-list">
-                    <div class="experiences">
+                    <div id="internship2">
                         <h3>IYA Limited</h3>
-                        <img class="pic0" src={IyaLimited} alt="iya-limited" />
-                        <img id="vietnam" src={Vietnam} alt="vietnam" />
+                        <img src={IyaLimited} alt="iya-limited" width={'10%'} />
+                        <img src={Vietnam} alt="vietnam" width={'5%'} />
 					    <p>3 Juin 2021</p> 
                         <p><span>Date :</span> 06/2021 - 08/2021</p>
                         <p><span>Poste :</span> Stagiaire en développement web</p>
@@ -75,10 +106,10 @@ const AboutMe = () => {
                         <p>❍ Rédaction d'un document de spécification: c'est-à-dire réaliser des diagrammes d'activité, diagrammes de cas d'utilisation, dictionnaires de données, wireframes pour chaque fonctionnalité.</p>
                         <p>❍ Rédaction d'un guide d'utilisation du site.</p> 
 					</div> 
-					<div class="experiences">
+					<div id="warehouseman">
                         <h3>SIMASTOCK</h3>
-                        <img class="pic0" src={Simastock} alt="simastock" />
-                        <img id="manutentionnaire" src={Manutentionnaire} alt="manutentionnaire" />
+                        <img src={Simastock} alt="simastock" width={'15%'} />
+                        <img src={Manutentionnaire} alt="manutentionnaire" width={'10%'} />
                         <p>Mars 2017</p>
                         <p><span>Date :</span> 03/2017 - 08/2017</p>
                         <p><span>Poste :</span> Manutentionnaire</p>
@@ -87,10 +118,10 @@ const AboutMe = () => {
                         <p>❍ Conditionnement, palettisation</p>
                         <p>❍ Picking, nettoyage</p>
 					</div>
-					<div class="experiences">
+					<div id="production-worker">
                         <h3>Wienerberger</h3> 
-                        <img class="pic0" src={Wienerberger} alt="wienerberger" />
-                        <img class="pic0" src={AgentDeProduction} alt="agent-de-production" />
+                        <img src={Wienerberger} alt="wienerberger" width={'15%'} />
+                        <img src={AgentDeProduction} alt="agent-de-production" width={'15%'} />
                         <p>Novembre 2016</p>
                         <p><span>Date :</span> 11/2016 - 12/2016</p>
                         <p><span>Poste :</span> Agent de production</p>
@@ -99,9 +130,9 @@ const AboutMe = () => {
                         <p>❍ Contrôle visuel</p>
                         <p>❍ Conditionnement</p>
 					</div>
-					<div class="experiences">
+					<div id="internship1">
                         <h3>Nord France Constructions</h3> 
-                        <img class="pic0" src={NordFranceConstructions} alt="nord-france-constructions" />
+                        <img src={NordFranceConstructions} alt="nord-france-constructions" width={'15%'} />
                         <p>11 mai 2015</p> 
                         <p><span>Date :</span> 05/2015 - 07/2015</p>
                         <p><span>Poste :</span> Stagiaire assistant chef de chantier</p>
@@ -113,6 +144,71 @@ const AboutMe = () => {
                         <a href="https://www.adele.org/residence/2480/maison-blanche-2480/lille-59" target="_blank" class="btn-experiences">Pour plus d'informations</a>
 					</div>
                 </div> 
+
+                <div id="skills-list">
+				    <h2>Mes compétences</h2>
+				    <div id="languages">
+					    <h3>Langues</h3>					
+                        <img src={France} alt="france" title="Français (courant)" />
+                        <img src={Angleterre} alt="angleterre" title="Anglais (niveau B2)" />
+                        <img src={USA} alt="usa" title="Anglais (niveau B2)" />
+                        <img src={Espagne} alt="espagne" title="Espagnol (notions)" /> 
+				    </div>
+				    <div id="programming-languages">
+					    <h3>Langages de programmation</h3>
+                        <div class="skills">
+                            <span>Programmation :</span>
+                            <img src={CCpp} alt="c-cpp" title="C/C++" />
+                            <img src={CSharp} alt="csharp" title="C#" />
+                            <img src={Python} alt="python" title="Python" />
+                            <img src={Java} alt="java" title="Java" />
+                            </div>
+                        <div class="skills">
+                            <span>Web :</span>
+                            <img src={HtmlCss} alt="html-css" title="HTML/CSS" />
+                            <img src={Php} alt="php" title="PHP" />
+                            <img src={Laravel} alt="laravel" title="Laravel" />
+                            <img src={Angular} alt="angular" title="Angular" />
+                            <img src={ReactJS} alt="react-js" title="React.js" />
+                            <img src={JavaScript} alt="javascript" title="JavaScript" />
+                            <img src={TypeScript} alt="typescript" title="TypeScript" />
+                            </div>
+                        <div class="skills">
+                            <span>Interfaces graphiques :</span>
+                            <img src={XAML} alt="xaml" title="XAML" />
+                            <img src={VisualBasic} alt="visual-basic" title="Visual Basic" />
+                            <img src={PythonTkinter} alt="python-tkinter" title="Tkinter" />
+                        </div>
+                        <div class="skills">
+                            <span>Bases de données :</span>
+                            <img src={MySQL} alt="mysql" title="MySQL" />
+                            <img src={PostgreSQL} alt="postgresql" title="PostgreSQL" />
+                            <img src={NoSQL} alt="nosql" title="NoSQL" />			
+                        </div>
+                    </div>
+                    <div id="softwares">
+                        <h3>Logiciels</h3>
+                        <div class="skills">
+					        <span>Developpement informatique :</span>
+                            <img src={VisualStudioCode} alt="visual-studio-code" title="Visual Studio Code" />
+                            <img src={VisualStudio} alt="visual-studio" title="Visual Studio" />
+                            <img src={IntelliJ} alt="intellij" title="IntelliJ IDEA" />
+                            <img src={PhpStorm} alt="php-storm" title="PHP Storm" />
+                            <img src={Git} alt="git" title="Git" />
+                        </div>
+                        <div class="skills">
+                            <span>Outils d'organisation :</span>
+                            <img src={Trello} alt="trello" title="Trello" />
+                            <img src={JiraSoftware} alt="jira-software" title="Jira software" />
+                        </div>
+                        <div class="skills">
+                            <span>Architecture :</span> 
+                            <img src={AutoCAD} alt="autocad" title="AutoCAD" />
+                            <img src={RevitArchitecture} alt="revit-architecture" title="Revit Architecture" />	
+                            <img src={SketchUp} alt="sketchup" title="SketchUp" />		
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
