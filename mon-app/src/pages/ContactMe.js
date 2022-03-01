be6e326785f0583492007b66712e576fcd93843a
JeremPineau <pi.jeremieoutlook.fr>
2022-03-01 13:43:26 +0100

    Add: elements page me contacter +Fix

--- mon-app/src/pages/ContactMe.js
+++ mon-app/src/pages/ContactMe.js
@@ -6,13 +6,18 @@ const ContactMe = () => {
         <div className="contact-me">
             <Navigation />
             <h1>Me contacter</h1> 
-            <p>
-                Eu ullamco aliquip ipsum esse quis. Ad tempor laborum id laborum commodo dolore excepteur nostrud mollit sunt labore culpa in. Sint anim esse proident qui mollit sint pariatur ea. Et nostrud laborum mollit ut qui in velit eiusmod veniam aliqua mollit dolore. Minim duis commodo non tempor. Est proident est id irure laboris incididunt duis.
-                Ex sit adipisicing incididunt minim amet nostrud ad voluptate velit labore pariatur. Deserunt exercitation irure esse reprehenderit duis amet. Nulla ut irure qui sint esse exercitation incididunt tempor minim nisi nostrud. Eu consectetur minim cupidatat aute quis do ipsum. Aute fugiat qui labore ullamco et irure. Lorem ex veniam cupidatat adipisicing elit sit ullamco.
-                Fugiat irure laborum mollit exercitation veniam fugiat dolore id adipisicing ut commodo id laborum pariatur. Nulla ipsum commodo aute aute nulla ullamco nostrud do. Ex sint ex proident exercitation Lorem. Ullamco ipsum commodo dolore mollit id. Ex nisi nulla veniam incididunt cillum est id in sunt anim laboris eu dolore. Eu Lorem et pariatur et occaecat non sit laborum irure tempor. Deserunt et incididunt commodo id nostrud dolore ad.
-                Voluptate culpa tempor ad voluptate tempor consectetur minim in aliquip aliqua. Amet anim et ut do ea aliqua deserunt ad qui duis laborum officia. Minim voluptate consectetur sunt quis ut eu exercitation qui ullamco fugiat nisi. Ad sit non incididunt commodo. Non in nulla adipisicing officia. Qui sint quis dolore mollit qui duis ut cupidatat esse.
-                Sunt ex excepteur officia adipisicing duis qui minim id id irure occaecat. Excepteur eu eu elit velit pariatur do. Ullamco exercitation voluptate consequat do proident sunt veniam proident incididunt voluptate. Reprehenderit laborum eu cupidatat in do. Excepteur cupidatat in minim culpa ullamco quis nostrud culpa esse sint qui id eiusmod elit. Cupidatat sunt ut deserunt labore proident id.
-            </p>
+
+            <div id="section">
+                <div id="contact">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20339.256643371868!2d2.9975263785286064!3d50.41488668609547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2ccf4870ba1cd%3A0x40af13e8163f5a0!2sCourcelles-l%C3%A8s-Lens!5e0!3m2!1sfr!2sfr!4v1604072137712!5m2!1sfr!2sfr" width={'100%'} />
+                    <p><span>Lieu :</span> Courcelles-lès-lens</p>
+                    <p><span>Tél :</span> 06 47 98 62 73</p>
+                    <p><span>Mail :</span><a href="mailto:pi.jeremie@outlook.fr" target="_blank"> pi.jeremie@outlook.fr</a></p>
+                    <p><span>LinkedIn :</span><a href="https://fr.linkedin.com/in/jeremiepineau13" target="_blank"> jeremiepineau13</a></p>
+                    <p><span>GitHub :</span><a href="https://github.com/JeremPineau" target="_blank"> JeremPineau</a></p>
+                    <p><span>GitLab :</span><a href="https://gitlab.com/JeremPineau" target="_blank"> JeremPineau</a></p>
+                </div>
+            </div>
         </div>
     );
 };
--- mon-app/src/pages/Homepage.js
+++ mon-app/src/pages/Homepage.js
@@ -7,15 +7,18 @@ const Homepage = () => {
         <div className="homepage">
             <Navigation />
             <h1>Bienvenue sur mon portfolio !</h1>
-            <div id="presentation">
-                <div id="jeremie">
-                    <img src={Jeremie} alt="jeremie" width={'60%'} />
-                </div>
-                <div id="description">
-                    <h2>Jérémie Pineau</h2> 
-                    <p>Passionné par le développement web, je souhaiterais vivre de ma passion.</p>
-                    <p>Mes diverses expériences m'ont apporté beaucoup de connaissances que je souhaite, aujourd'hui, mettre à contribution en intégrant une équipe de devéloppeurs.</p>
-                    <p>Motivation, perfectionnisme et soif d'apprendre sont les adjectifs qui me définissent le mieux, comme en témoigne mon parcours scolaire plutôt atypique.</p>
+
+            <div id="section">
+                <div id="presentation">
+                    <div id="jeremie">
+                        <img src={Jeremie} alt="jeremie" width={'60%'} />
+                    </div>
+                    <div id="description">
+                        <h2>Jérémie Pineau</h2> 
+                        <p>Passionné par le développement web, je souhaiterais vivre de ma passion.</p>
+                        <p>Mes diverses expériences m'ont apporté beaucoup de connaissances que je souhaite, aujourd'hui, mettre à contribution en intégrant une équipe de devéloppeurs.</p>
+                        <p>Motivation, perfectionnisme et soif d'apprendre sont les adjectifs qui me définissent le mieux, comme en témoigne mon parcours scolaire plutôt atypique.</p>
+                    </div>
                 </div>
             </div>
         </div>
