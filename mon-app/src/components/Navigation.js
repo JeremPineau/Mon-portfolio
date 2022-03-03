76402855e4eba31d35a4c2ef59d14247b3f369a8
JeremPineau <pi.jeremieoutlook.fr>
2022-03-03 07:14:06 +0100

    Fix

--- mon-app/src/components/Navigation.js
+++ mon-app/src/components/Navigation.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Cat from '../assets/cat.png';
+import Cat from '../assets/navigation/cat.png';
+import Linkedin from '../assets/navigation/linkedin.png';
+import Github from '../assets/navigation/github.png';
+import Gitlab from '../assets/navigation/gitlab.png';
 
 const Navigation = () => {
     return (
         <div className="navigation">
             <div id="nav">
                 <img id="cat" src={Cat} alt="cat" />
+                <a href="https://fr.linkedin.com/in/jeremiepineau13" target="_blank"><img id="linkedin" src={Linkedin} alt="linkedin" width={'30em'} /></a>
+                <a href="https://github.com/JeremPineau" target="_blank"><img id="github" src={Github} alt="github" width={'40em'} /></a>
+                <a href="https://gitlab.com/JeremPineau" target="_blank"><img id="gitlab" src={Gitlab} alt="gitlab" width={'25em'} /></a>
                 <NavLink id="contact-me-nav" exact to='/contact-me' activeClassName="nav-active">
                     <li>Me contacter</li>
                 </NavLink>
--- mon-app/src/pages/ContactMe.js
+++ mon-app/src/pages/ContactMe.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Mail from '../assets/contact-me/mail.png';
+import Phone from '../assets/contact-me/telephone.png';
 import FooterFixed from '../components/FooterFixed';
 
 const ContactMe = () => {
@@ -9,17 +11,18 @@ const ContactMe = () => {
             <h1>Me contacter</h1> 
 
             <div id="section">
-                <div id="contact">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20339.256643371868!2d2.9975263785286064!3d50.41488668609547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2ccf4870ba1cd%3A0x40af13e8163f5a0!2sCourcelles-l%C3%A8s-Lens!5e0!3m2!1sfr!2sfr!4v1604072137712!5m2!1sfr!2sfr" width={'100%'} />
-                    <div id="informations">
-                        <p><span>Lieu :</span> Courcelles-lès-lens</p>
-                        <p><span>Tél :</span> 06 47 98 62 73</p>
-                        <p><span>Mail :</span><a href="mailto:pi.jeremie@outlook.fr" target="_blank"> pi.jeremie@outlook.fr</a></p>
-                        <p><span>LinkedIn :</span><a href="https://fr.linkedin.com/in/jeremiepineau13" target="_blank"> jeremiepineau13</a></p>
-                        <p><span>GitHub :</span><a href="https://github.com/JeremPineau" target="_blank"> JeremPineau</a></p>
-                        <p><span>GitLab :</span><a href="https://gitlab.com/JeremPineau" target="_blank"> JeremPineau</a></p>
+                    <div id="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20339.256643371868!2d2.9975263785286064!3d50.41488668609547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2ccf4870ba1cd%3A0x40af13e8163f5a0!2sCourcelles-l%C3%A8s-Lens!5e0!3m2!1sfr!2sfr!4v1604072137712!5m2!1sfr!2sfr" width={'40%'} /> 
                     </div>
-                </div>
+                    <div id="informations">
+                        <div id="mail">
+                            <img src={Mail} alt="mail" width={'20%'} />&emsp;<a href="mailto:pi.jeremie@outlook.fr" target="_blank">pi.jeremie@outlook.fr</a>
+                        </div>
+                        <div id="phone">
+                            <img src={Phone} alt="phone" width={'20%'} />&emsp;<p>06 47 98 62 73</p>
+                        </div>
+                    </div>
+        
             </div>
             <FooterFixed />
         </div>
