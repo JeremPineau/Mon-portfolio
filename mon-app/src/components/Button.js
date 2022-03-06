0ae04b7e323cfed9fbcae637e41b84008414e857
JeremPineau <pi.jeremieoutlook.fr>
2022-03-06 10:32:58 +0100

    Fix

--- mon-app/src/components/Button.js
+++ mon-app/src/components/Button.js
@@ -7,39 +7,7 @@ export const Button = styled.div`
    z-index: 1;
    cursor: pointer;
    color: rgb(70,130,180);
-    
-   @media(min-width: 275px) and (max-width: 400px) {
-      font-size: .25em;
-      bottom: 3em;
-   }
-  
-   @media(min-width: 400px) and (max-width: 600px) {  
-      font-size: .5em;
-      bottom: 2.3em;
-   }
-  
-   @media(min-width: 600px) and (max-width: 800px) {
-      font-size: 1em;
-      bottom: 1.5em;
-   }
-  
-   @media(min-width: 800px) and (max-width: 1000px) {
-      font-size: 2em;
-      bottom: .9em;
-   }
-  
-   @media(min-width: 1000px) and (max-width: 1200px) {
-      font-size: 3em;
-      bottom: .7em;
-   }
-  
-   @media(min-width: 1200px) and (max-width: 1400px) {
-      font-size: 4em;
-      bottom: .6em;
-   }
-  
-   @media(min-width: 1400px) {
-      font-size: 5em;
-      bottom: .5em;
-   }
+   font-size: 4vw;
+   bottom: 3vw;
+   opacity: 70%;
 `
